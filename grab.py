e9ba14ddf51bea3db1a4b651e3028dcd3ee3835e
noobcoder99 <jofmc858@gmail.com>
2019-01-11 02:48:12 -0500

    Add files via upload

---
+++ grab.py
@@ -0,0 +1,10 @@
+import socket
+s = socket.socket()
+
+try:
+    s.connect(("packtpub.samsclass.info", 22))
+    print s.recv(1024)
+    s.close()
+# error statement is printed if connection not made or refused
+except socket.error as err:
+    print err
